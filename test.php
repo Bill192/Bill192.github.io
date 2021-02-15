e7880678be6f9e17b07deafaba6095405aa9b510
Bill192 <76957812+Bill192@users.noreply.github.com>
2021-02-14 21:19:08 -0500

    Add files via upload

---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+years = array();
+                for ($x=1; $x <= $bean->10 ; $x++) {
+
+                  array_push(years, (500 * (pow(1.03, $x)) );
+
+                }
+
+print_r(years);
