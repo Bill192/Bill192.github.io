381af53b16f07baa0e1aeabc20a90a2bdb1f9482
Bill192 <76957812+Bill192@users.noreply.github.com>
2021-02-14 21:22:02 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+years = array();
+                for ($x=1; $x <= $bean->10 ; $x++) {
+
+                  array_push(years = (500 * (pow(1.03, $x)) );
+
+                }
+echo "years";
+print_r(years);
